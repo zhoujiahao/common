658a8814a65f047b3515dd4b09fec7a04031eadd
zhoujiahao <zhoujiahao@users.noreply.github.com>
2018-05-11 14:34:54 +0800

    shellUtil修改
    处理标注输出和error输出

--- common-utils/src/main/java/cn/zhou/utils/ShellUtil.java
+++ common-utils/src/main/java/cn/zhou/utils/ShellUtil.java
@@ -5,34 +5,70 @@ import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ShellUtil {
 
     private static Logger logger = LoggerFactory.getLogger(ShellUtil.class);
 
-    private ShellUtil() {
-    }
-
     public static int exec(String cmd) throws Exception {
         logger.info("***** exec cmd : " + cmd + " *****");
         Process exec = Runtime.getRuntime().exec(new String[]{"/bin/bash", "-c", cmd});
+        return waitForResult(exec);
+    }
+
+    public static int waitForResult(Process exec) throws Exception {
+        //读取标准错误流
+        BufferedReader brError = new BufferedReader(new InputStreamReader(exec.getErrorStream(), "utf8"));
+        //读取标准输入流
         BufferedReader br = new BufferedReader(new InputStreamReader(exec.getInputStream()));
-        String line;
-        while ((line = br.readLine()) != null) {
-            logger.error("************* INFO ************" + line);
-        }
+        Thread t1 = new Thread() {
+            public void run() {
+                String line = null;
+                try {
+                    while ((line = brError.readLine()) != null) {
+                        System.out.println("***** ERROR *****  " + line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (brError != null)
+                            brError.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        Thread t2 = new Thread() {
+            public void run() {
+                String line = null;
+                try {
+                    while ((line = br.readLine()) != null) {
+                        System.out.println("***** INFO  *****  " + line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (br != null)
+                            br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t1.start();
+        t2.start();
         return exec.waitFor();
     }
 
     public static int exec(String dir, String cmd) throws Exception {
         logger.info("***** In dir : " + dir + " , exec cmd : " + cmd + " ***");
         Process exec = Runtime.getRuntime().exec(new String[]{"/bin/bash", "-c", cmd}, null, new File(dir));
-        BufferedReader br = new BufferedReader(new InputStreamReader(exec.getInputStream()));
-        String line;
-        while ((line = br.readLine()) != null) {
-            logger.error("************* INFO ************" + line);
-        }
-        return exec.waitFor();
+        return waitForResult(exec);
     }
 }
